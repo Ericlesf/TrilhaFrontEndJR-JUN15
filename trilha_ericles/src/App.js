0c671cf60db24312c743e0e2d48617361ceb5778
Ericlesf <ericles@outlook.com>
2024-06-23 01:53:23 -0300

    Responsividade no corpo.

--- trilha_ericles/src/App.js
+++ trilha_ericles/src/App.js
@@ -9,8 +9,6 @@ function App() {
 
       <Header />
 
-      
-
       <header className="App-header">
         <img src={perfil} className="perfil-logo" alt="perfil" />
 
@@ -22,8 +20,8 @@ function App() {
           Desenvolvedor Front-End
         </h2>
 
-        <p>
-          Busco uma oportunidade como desenvolvedor front-end ou mobile, onde possa aplicar e expandir minhas habilidades em HTML, CSS e JavaScript, além de bibliotecas modernas como React e React Native. Com um forte foco na criação de interfaces intuitivas e experiências de usuário envolventes, almejo integrar uma equipe de desenvolvimento inovadora. Meu objetivo é utilizar boas práticas de design para desenvolver soluções eficientes, acessíveis e impactantes. Estou continuamente aprimorando minhas competências técnicas e buscando novos desafios que me permitam aprender e contribuir de forma significativa para projetos dinâmicos e de alta performance.
+        <p className="about">
+          Me chamo Éricles, tenho 27 anos, e busco uma oportunidade como desenvolvedor front-end ou mobile, onde possa aplicar e expandir minhas habilidades em HTML, CSS e JavaScript, além de bibliotecas modernas como React e React Native. Com um forte foco na criação de interfaces intuitivas e experiências de usuário envolventes, almejo integrar uma equipe de desenvolvimento inovadora. Meu objetivo é utilizar boas práticas de design para desenvolver soluções eficientes, acessíveis e impactantes. Estou continuamente aprimorando minhas competências técnicas e buscando novos desafios que me permitam aprender e contribuir de forma significativa para projetos dinâmicos e de alta performance.
         </p>
       </header>
     </div>
